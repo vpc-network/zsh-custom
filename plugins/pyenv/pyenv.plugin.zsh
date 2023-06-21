6e56841f63a75c33bc86cd98178adb2210229c10
sng <sng@develo.pe>
2023-06-21 17:04:31 +0000

    Update nvm and pyenv plugins

--- plugins/pyenv/pyenv.plugin.zsh
+++ plugins/pyenv/pyenv.plugin.zsh
@@ -1,3 +1,10 @@
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
+export PYENV_DIR="$HOME/.pyenv"
+
+if [ -d $PYENV_DIR/bin ]; then
+  # export to path
+  export PATH="$PYENV_DIR/bin:$PATH"
+
+  # autoload pyenv
+  eval "$(pyenv init --path)"
+  eval "$(pyenv virtualenv-init -)"
+fi
